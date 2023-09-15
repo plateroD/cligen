cb426228e85181e2829efeffe3db8454127f05da
plateroD <143241489+plateroD@users.noreply.github.com>
2023-09-15 09:56:55 -0500

    Add files via upload

--- cligen calcuations.R
+++ cligen calcuations.R
@@ -1,6 +1,5 @@
 FL1
 
-# Assuming your data frame is named FL1
 # Convert the 'mo' column to a factor to ensure it considers all months
 FL1$mo <- factor(FL1$mo, levels = 1:12)
 
@@ -72,7 +71,7 @@ min_avg_rad <- min(aggregate(rad ~ mo, data = FL1, FUN = mean)$rad)
 cat("Maximum Average Radiation (W/m**2):", max_avg_rad, "\n")
 cat("Minimum Average Radiation (W/m**2):", min_avg_rad, "\n")
 
-***MD
+###MD
 MD1=read.csv("MD.csv")
 # Filter out the rows with valid numeric values for the "rad" column
 MD1 <- MD1[complete.cases(MD1$rad), ]
@@ -100,9 +99,86 @@ cat("Maximum Wind Speed (m/s):", max_wvl, "\n")
 cat("Minimum Wind Speed (m/s):", min_wvl, "\n")
 cat("Average Wind Speed (m/s):", avg_wvl, "\n")
 
+###10% decrease in wet days calc.
+NM1 <- read.csv("NM .csv")
+total_wet_days <- sum(NM1$prcp > 0)
+decrease_10_percent <- total_wet_days * 0.10
+new_wet_days <- total_wet_days - decrease_10_percent
+new_wet_days
+
+###10% increase
+# Convert "prcp" column to numeric
+NM1$prcp <- as.numeric(NM1$prcp)
+
+# Check for any NA or missing values in the "prcp" column
+if (any(is.na(NM1$prcp))) {
+  # If there are missing values, you may want to handle them appropriately
+  # For now, we'll assume you want to remove rows with missing values
+  NM1 <- NM1[!is.na(NM1$prcp), ]
+}
+
+# Calculate the mean daily precipitation for all months
+mean_daily_precipitation <- mean(NM1$prcp)
+
+# Calculate a 10% increase in mean daily precipitation
+increase_10_percent <- mean_daily_precipitation * 0.10
+
+# Calculate the new mean daily precipitation after the 10% increase
+new_mean_daily_precipitation <- mean_daily_precipitation + increase_10_percent
+
+# Print the new mean daily precipitation
+new_mean_daily_precipitation
+
+###MX.5P
+MX<-read.csv("MX5P.csv")
+# Define the percentage increase
+percentage_increase <- 0.10
+
+# Apply the 10% increase to the 'MX.5P.NM' column in the 'MX' data frame
+MX$MX.5P.NM <- MX$MX.5P.NM * (1 + percentage_increase)
+
+# Print the updated 'MX' data frame
+print(MX)
+
+###temp increase by 5C
+# Define the temperature increase
+temperature_increase <- 5
+
+# Add 5°C to both 'tmax' and 'tmin' columns in the 'NM1' data frame
+NM1$tmax <- NM1$tmax + temperature_increase
+NM1$tmin <- NM1$tmin + temperature_increase
+
+# Print the updated 'NM1' data frame
+print(NM1)
+
+# Load the dplyr library if it's not already loaded
+# install.packages("dplyr")
+library(dplyr)
+
+# Assuming 'NM1' contains your data frame
+# First, add 5°C to both 'tmax' and 'tmin' columns as previously shown
+temperature_increase <- 5
+NM1$tmax <- NM1$tmax + temperature_increase
+NM1$tmin <- NM1$tmin + temperature_increase
+
+# Calculate the overall average temperature for all months
+overall_average_temperature <- NM1 %>%
+  summarise(
+    AvgTmax = mean(tmax, na.rm = TRUE),
+    AvgTmin = mean(tmin, na.rm = TRUE)
+  )
+
+# Print the overall average temperature
+print(overall_average_temperature)
 
 
 
+library(dplyr)
 
+# Assuming you already have the NM1 dataset loaded
 
+# Decrease precipitation by 10% for all rows
+NM1$prcp <- NM1$prcp * 0.90  # Decrease by 10%
 
+# Export the modified dataset as a CSV file
+write.csv(NM1, file = "NM1_modified_ten_decrease.csv", row.names = FALSE)
